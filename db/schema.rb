ecc821bfbcb8d71e45c99737cd2f6d28ad97c6af
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-05 22:32:34 +0900

    マイグレーションファイルを実行して、blogs テーブルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_000542) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_083601) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
