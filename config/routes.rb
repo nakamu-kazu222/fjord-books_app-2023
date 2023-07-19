b6b8f5479bc3461f7aeec5c8a5157411feb8624d
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-19 17:20:16 +0900

    resource をリソースを使用せずに表現した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :books
-  resources :blogs
+
+  get '/blogs', to: 'blogs#index', as: 'blogs'
+  get '/blogs/new', to: 'blogs#new', as: 'new_blog'
+  post '/blogs', to: 'blogs#create'
+  get '/blogs/:id', to: 'blogs#show', as: 'blog'
+  get '/blogs/:id/edit', to: 'blogs#edit', as: 'edit_blog'
+  patch '/blogs/:id', to: 'blogs#update'
+  delete '/blogs/:id', to: 'blogs#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
