14343a0d7afb3612f75f84d6fbb9851ea12349c2
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-21 19:06:45 +0900

    numericality で属性に数値のみが使われていることを検証する

--- app/models/admin/blog.rb
+++ app/models/admin/blog.rb
@@ -1,3 +1,3 @@
 class Admin::Blog < ApplicationRecord
-  validates :content, format: { with: /\A[a-zA-Z]+\z/,message: "は、英文字のみが使えます" }
+  validates :content, numericality: { only_integer: true }
 end
