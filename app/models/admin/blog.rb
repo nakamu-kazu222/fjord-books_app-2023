497ba1bb7b14913413e63fae8216cdf3a3c4d2c8
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-21 20:08:14 +0900

    ifで条件を指定して、空の場合のみバリデーションエラーが発生するようにした

--- app/models/admin/blog.rb
+++ app/models/admin/blog.rb
@@ -1,3 +1,7 @@
 class Admin::Blog < ApplicationRecord
-  validates :title, :content, presence: true
+  validates :title, :content, presence: true, if: :title_or_content_present?
+
+  def title_or_content_present?
+    title.present? || content.present?
+  end
 end
