efdc60a9636bc86b8cf728b5dad3db9a01f8c36b
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-26 15:45:30 +0900

    rescue_fromを使用して、404エラーメッセージを表示する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+  def record_not_found
+    render plain: "404 Not Found!", status: 404
+  end
 end
