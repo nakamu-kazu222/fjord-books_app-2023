3e3cb8856b744b5830863719fb9c4be03db75aa4
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-07-26 16:13:42 +0900

    トランザクションを使用して、削除されたときの処理を追加した

--- app/models/admin/blog.rb
+++ app/models/admin/blog.rb
@@ -1,5 +1,15 @@
 class Admin::Blog < ApplicationRecord
   include ActiveModel::Validations
   validates :title, :content, presence: true
-  # validates_with MyValidator
+  validates_with MyValidator
+
+  after_commit :delete_blog, on: :destroy
+
+  def delete_blog
+    if destroyed?
+      puts '削除された'
+    else
+      puts '削除に失敗した'
+    end
+  end
 end
