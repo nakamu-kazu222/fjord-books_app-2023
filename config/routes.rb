75ab6fa0cbe40036e2927cb1602385cc78f8bdbf
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-08-01 20:21:40 +0900

    ユーザー一覧画面を表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   resources :books
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  get "users/show" => "users#show"
-
-
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get "users/index" => "users#index", as: :users
+  get "users/:id" => "users#show", as: :user
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
