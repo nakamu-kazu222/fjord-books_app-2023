723e187c030384cff091dab676c43f3ade5a9988
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-08-01 20:58:58 +0900

    ページング処理を追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(2)
   end
 
   def show
