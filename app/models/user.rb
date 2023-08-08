4975f7fd0b8513d9948acc25ab5f371d9ff70067
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-08-08 18:09:01 +0900

    アップロード可能なファイルをjpg, gif, pngにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates :name, presence: true
+  validate :verify_file_type
+  has_one_attached :user_icon
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one_attached :user_icon
+
+  private
+
+  def verify_file_type
+    return unless user_icon.attached?
+
+    allowed_file_types = %w[image/jpg image/gif image/png]
+    errors.add(:user_icon, 'only jpg, gif, png') unless allowed_file_types.include?(user_icon.blob.content_type)
+  end
 end
