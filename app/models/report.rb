998c3105276139f0eaf3b6064d6fb8999e6db208
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-08-23 16:20:50 +0900

    Comment モデルとポリモーフィック関連付けをした

---
+++ app/models/report.rb
@@ -0,0 +1,8 @@
+class Report < ApplicationRecord
+  belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+end
