4351f6c605c22dc8073cb1bab71753ba23bb166c
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-08-23 16:26:58 +0900

    Reports::Commentsコントローラーを作成した

---
+++ app/controllers/reports/comments_controller.rb
@@ -0,0 +1,14 @@
+class Reports::CommentsController < CommentsController
+  before_action :set_commentable
+
+  private
+
+  def set_commentable
+    @commentable = Report.find(params[:report_id])
+  end
+
+  def render_commentable_show
+    @report = @commentable
+    render 'reports/show'
+  end
+end
