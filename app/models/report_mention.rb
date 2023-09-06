bd877d1d54a4744eaca4ae4b2720f521b27231df
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-09-06 11:18:28 +0900

    Reportモデル間の言及の関連性を持たせた
    mentioning_report と mentioned_report で2つの日報の関連を通じて、日報間の言及を表現した

---
+++ app/models/report_mention.rb
@@ -0,0 +1,4 @@
+class ReportMention < ApplicationRecord
+  belongs_to :mentioning_report, class_name: "Report", foreign_key: "mentioning_report_id"
+  belongs_to :mentioned_report, class_name: "Report", foreign_key: "mentioned_report_id"
+end
