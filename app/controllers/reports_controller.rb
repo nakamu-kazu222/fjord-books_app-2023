b4279d40412ad82d2857ba860630783461ce42c5
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-09-06 11:28:20 +0900

    日報を新規作成、編集、削除したときに、日報の本文から言及された他の日報を関連を設定する
    destroy メソッドでmentioning_report と mentioned_report の関連を持つレコードを削除する

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,27 +19,26 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = current_user.reports.new(report_params)
-
-    if @report.save
-      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @report = current_user.reports.new(report_params)        
+    save_report_and_mention(:new)
   end
 
   def update
     if @report.update(report_params)
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
+      save_report_and_mention(:edit)    
     end
   end
 
   def destroy
-    @report.destroy
-
+    ActiveRecord::Base.transaction do
+      ReportMention.where(mentioning_report: @report).destroy_all
+      ReportMention.where(mentioned_report: @report).destroy_all
+      @report.destroy
+    end
+  
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+  rescue ActiveRecord::InvalidForeignKey
+    redirect_to reports_url, alert: '日報を削除できませんでした。'
   end
 
   private
@@ -51,4 +50,26 @@ class ReportsController < ApplicationController
   def report_params
     params.require(:report).permit(:title, :content)
   end
+end
+
+def extract_mentioned_report_ids(text)
+  url_pattern = /http:\/\/localhost:3000\/reports\/(\d+)/
+  text.scan(url_pattern).flatten.map(&:to_i)
+end
+
+
+def save_report_and_mention(view)
+  if @report.save
+    mentioned_report_ids = extract_mentioned_report_ids(@report.content)
+    mentioned_reports = Report.where(id: mentioned_report_ids)
+    
+    mentioned_reports.each do |mentioned_report|
+      mentioned_report.mentioned_reports << @report
+      mentioned_report.save
+    end
+
+    redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+  else
+    render view, status: :unprocessable_entity
+  end  
 end
