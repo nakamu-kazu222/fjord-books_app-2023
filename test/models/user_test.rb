e24765d7d48238b4b7708750ec9e48813851e7f5
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-10-03 19:26:48 +0900

    Userクラスのテストコードを作成した
    ユーザー名、メールのテストを行う

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user1)
+  end
+
+  test 'user_name_or_email' do
+    user = User.new(email: 'test@test', name: '')
+    assert_equal 'user1', @user.name_or_email
+    assert_equal 'test@test', user.name_or_email
+  end
 end
