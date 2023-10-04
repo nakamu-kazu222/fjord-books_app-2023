ff7d1b3056624d519ef59f94965f4405e114bf13
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-10-04 13:23:54 +0900

    Reportモデルテストを作成した
    ログインしているユーザーと指定したユーザーが一致するかテスト
    日報を作成した日時が一致するかテスト
    日報を作成した時の言及有無をテスト
    日報を削除した時のテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,34 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+    @user3 = users(:user3)
+    @report_user1 = reports(:report_user1)
+    @report_user2 = reports(:report_user2)
+    @report_user3 = reports(:report_user3)
+  end
+
+  test 'editable?' do
+    assert @report_user1.editable?(@user1)
+    assert_not @report_user1.editable?(@user3)
+  end
+
+  test 'created_on' do
+    assert_equal Date.parse('Wed, 04 Oct 2023'), @report_user1.created_on
+    assert_not_equal Date.parse('Tue, 10 Oct 2023'), @report_user1.created_on
+  end
+
+  test 'save_mentions' do
+    assert_equal [], @report_user1.send(:save_mentions)
+
+    assert_equal [@report_user1], @report_user3.send(:save_mentions)
+
+    @report_user3.update(content: 'http://localhost:3000/reports/1 http://localhost:3000/reports/2')
+    assert_equal [@report_user1, @report_user2], Report.find(@report_user3.id).send(:save_mentions)
+
+    @report_user3.update(content: 'http://localhost:3000/reports/1')
+    assert_equal [@report_user1], Report.find(@report_user3.id).send(:save_mentions)
+  end
 end
